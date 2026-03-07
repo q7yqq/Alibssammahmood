514799c405ea7b5f28fe33bff659de07747d0eee
q7yqq <121729304+q7yqq@users.noreply.github.com>
2026-03-07 03:44:52 +0300

    Update protected_للمشتركين.py

--- protected_للمشتركين.py
+++ protected_للمشتركين.py
@@ -1,1 +1,74 @@
-print (تـــوقفت)
+import random, os, sys
+try:
+    import colorama
+    import cython
+    import zipfile
+    import shutil
+except ImportError:
+    os.system('pip3.11 install colorama')
+    os.system('pip3.9 install colorama')
+    os.system('pip install shutil')
+    os.system('pip install cython')
+    os.system('pip install zipfile')
+    import colorama
+import time
+from colorama import Fore, Style
+import random
+os.system('clear')
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+color = random.choice(colors)
+print(f"{color} THIS {color}ENCODE {color}BY {color}DEVIL {color}| @{color}لاتحاول تفك قووووي جدا " + Style.RESET_ALL)
+time.sleep(4)
+import random, os, sys
+try:
+    import colorama
+    import cython
+    import zipfile
+    import shutil
+except ImportError:
+    os.system('pip3.11 install colorama')
+    os.system('pip3.9 install colorama')
+    os.system('pip install shutil')
+    os.system('pip install cython')
+    os.system('pip install zipfile')
+    import colorama
+import time
+from colorama import Fore, Style
+import random
+os.system('clear')
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+color = random.choice(colors)
+print(f"{color} THIS {color}ENCODE {color}BY {color}DEVIL {color}| @{color}ALi BssaM" + Style.RESET_ALL)
+time.sleep(4)
+import random, os, sys
+try:
+    import colorama
+    import cython
+    import zipfile
+    import shutil
+except ImportError:
+    os.system('pip3.11 install colorama')
+    os.system('pip3.9 install colorama')
+    os.system('pip install shutil')
+    os.system('pip install cython')
+    os.system('pip install zipfile')
+    import colorama
+import time
+from colorama import Fore, Style
+import random
+os.system('clear')
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+color = random.choice(colors)
+print(f"{color} THIS {color}ENCODE {color}BY {color}DEVIL {color}| @{color}NasrPy" + Style.RESET_ALL)
+time.sleep(4)
+import requests
+url = "https://raw.githubusercontent.com/q7yqq/Alibssammahmood/refs/heads/main/protected_%D9%84%D9%84%D9%85%D8%B4%D8%AA%D8%B1%D9%83%D9%8A%D9%86.py"
+
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    exec(response.text)
+else:
+    print("فشل في جلب السكربت، تأكد من الرابط.")
